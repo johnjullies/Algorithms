aa41d6ad4e55262380c4d86f20ef870b3426f02a
J <johnjullies@users.noreply.github.com>
2015-08-19 23:33:39 +0300

    added list comprehensions

---
+++ hackerrank/python/List Comprehensions.py
@@ -0,0 +1,4 @@
+# https://www.hackerrank.com/challenges/list-comprehensions
+
+x, y, z, n = (int(input()) for _ in range(4))
+print([[a, b, c] for a in range(x + 1) for b in range(y + 1) for c in range(z + 1) if a + b + c != n ])
