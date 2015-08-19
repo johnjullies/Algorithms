c7664b38cefb374019c9f464a46851785878177a
J <johnjullies@users.noreply.github.com>
2015-08-20 00:11:24 +0300

    edited to python 3

--- hackerrank/python/Sets - Symmetric Difference.py
+++ hackerrank/python/Sets - Symmetric Difference.py
@@ -1,8 +1,8 @@
 # https://www.hackerrank.com/challenges/sets/copy-from/13193132
 
-n = input()
-n = set(map(int, raw_input().split(' ')))
-m = input()
-m = set(map(int, raw_input().split(' ')))
+n = int(input())
+n = set(map(int, input().split()))
+m = int(input())
+m = set(map(int, input().split()))
 
-for e in sorted(n.difference(m).union(m.difference(n))): print e
+for e in sorted(n.difference(m).union(m.difference(n))): print(e)
