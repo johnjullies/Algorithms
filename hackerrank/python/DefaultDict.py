8273e5b11575f6dfef0a715ca745dd87caed29fb
J <johnjullies@users.noreply.github.com>
2015-08-22 23:53:19 +0300

    added defaultdict

---
+++ hackerrank/python/DefaultDict.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/defaultdict-tutorial
+
+from collections import defaultdict
+
+r = [int(n) for n in input().split()]
+d = defaultdict(list)
+
+for _ in range(r[0]):
+    d['a'].append(input())
+for _ in range(r[1]):
+    d['b'].append(input())
+
+for e in d['b']: 
+    if e in d['a']: 
+        indices = [i + 1 for i, x in enumerate(d['a']) if x == e]
+        print(' '.join([str(x) for x in indices]))
+    else:
+        print(-1)
