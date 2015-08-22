354e33cf16f922cae319d31da933f2e1a53a3143
J <johnjullies@users.noreply.github.com>
2015-08-23 00:02:19 +0300

    edited defaultdict +speed

--- hackerrank/python/DefaultDict.py
+++ hackerrank/python/DefaultDict.py
@@ -2,17 +2,21 @@
 
 from collections import defaultdict
 
-r = [int(n) for n in input().split()]
-d = defaultdict(list)
+def getIndices(d):
+    for e in d['b']: 
+        if e in d['a']: 
+            indices = [i + 1 for i, x in enumerate(d['a']) if x == e]
+            print(' '.join([str(x) for x in indices]))
+        else:
+            print(-1)
 
-for _ in range(r[0]):
-    d['a'].append(input())
-for _ in range(r[1]):
-    d['b'].append(input())
+if __name__ == "__main__":
+    r = [int(n) for n in input().split()]
+    d = defaultdict(list)
 
-for e in d['b']: 
-    if e in d['a']: 
-        indices = [i + 1 for i, x in enumerate(d['a']) if x == e]
-        print(' '.join([str(x) for x in indices]))
-    else:
-        print(-1)
+    for _ in range(r[0]):
+        d['a'].append(input())
+    for _ in range(r[1]):
+        d['b'].append(input())
+
+    getIndices(d)
