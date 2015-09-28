1ef8b3959a8fd3a653db1a4a7e32784dbca32c20
J <johnjullies@users.noreply.github.com>
2015-09-28 20:12:10 +0300

    improved logic

--- hackerrank/python/HTML Parser - Part 2.py
+++ hackerrank/python/HTML Parser - Part 2.py
@@ -4,12 +4,13 @@ from html.parser import HTMLParser
 
 class MyHTMLParser(HTMLParser):
     def handle_comment(self, data):
-        if '\n' not in data:
-            print('>>> Single-line Comment')
-            print(data)
-        elif '\n' in data:
-            print('>>> Multi-line Comment')
-            print(data)
+        if data != '\n':
+            if '\n' not in data:
+                print(">>> Single-line Comment")
+                print(data)
+            else:
+                print(">>> Multi-line Comment")
+                print(data)
             
     def handle_data(self, data):
         if data != '\n': 
