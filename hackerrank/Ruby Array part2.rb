951ace174f1d4f7fca93cf0e2f3d8400f6dcb559
John Jullies Palma <johnjullies@users.noreply.github.com>
2015-10-22 10:44:44 -0700

    Create Ruby Array part2.rb

---
+++ hackerrank/Ruby Array part2.rb
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/ruby-array-index-ii/
+
+def neg_pos(arr, index)
+    # return the element of the array at the position `index` from the end of the list
+    # Clue : arr[-index]
+    return arr[-index]
+end
+
+def first_element(arr)
+    # return the first element of the array
+    # arr.first
+    return arr.first
+end
+
+def last_element(arr)
+    # return the last element of the array
+    # arr.last
+    return arr.last
+end
+
+def first_n(arr, n)
+    # return the first n elements of the array
+    return arr.take n
+end
+
+def drop_n(arr, n)
+    # drop the first n elements of the array and return the rest
+    return arr.drop n
+end
