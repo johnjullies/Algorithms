a1d2679887de83028d1edcca6fecc092a44e826c
John Jullies Palma <johnjullies@users.noreply.github.com>
2016-01-17 23:55:34 +0800

    Update Day 0: Print Hello World!.js

--- hackerrank/contests/30DaysofCode/Day 0: Print Hello World!.js
+++ hackerrank/contests/30DaysofCode/Day 0: Print Hello World!.js
@@ -1,3 +1,5 @@
+// https://www.hackerrank.com/contests/30-days-of-code/challenges/day-0-print-hello-world
+
 function processData(input) {
     console.log("Hello World.\nWelcome to 30 Days of Code.");
 } 
