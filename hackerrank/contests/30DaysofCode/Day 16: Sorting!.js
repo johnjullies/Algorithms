47e075dddde4daf1fb29769eb3be11c5c04605de
John Jullies Palma <johnjullies@users.noreply.github.com>
2016-01-31 23:46:40 +0800

    working code; sort(function(a,b){return a-b})

--- hackerrank/contests/30DaysofCode/Day 16: Sorting!.js
+++ hackerrank/contests/30DaysofCode/Day 16: Sorting!.js
@@ -1,30 +1,37 @@
 // https://www.hackerrank.com/contests/30-days-of-code/challenges/day-16-closest-numbers
 
 function processData(input) {
-    var a_input = input.split("\n");
-    var n = Number(a_input[0]);
-    var a = a_input[1].split(" ").map(Number);
-    var min_diff = Number.MAX_SAFE_INTEGER;
-    var differences = []; 
-    
-    for (var i = 0; i < a.length-1; i++) {
-        for (var j = i+1; j < a.length; j++) {
-            var d = Math.abs(a[i] - a[j]);
-            if (d < min_diff) {
-                differences.length = 0;
-                min_diff = d;
-                differences.push(a[i], a[j]);
-            }
-            else if (d == min_diff) {
-                differences.push(a[i], a[j]);
+    var a = input.split('\n');
+    var myArray = a[1].split(' ').map(Number);
+    myArray.sort(function(a,b){return a - b});
+    var result = []
+    var diff = Infinity;
+    for (var i = 0; i < myArray.length-1; i++) {
+        if (Math.abs(myArray[i] - myArray[i+1]) <= diff) {
+            for (var j = i+1; j < myArray.length; j++) {
+                var d = Math.abs(myArray[i] - myArray[j]);
+                if (d < diff) {
+                    diff = d;
+                    result = Array(myArray[i], myArray[j]);
+                } else if (d === diff) {
+                    result.push(myArray[i], myArray[j]);
+                }
             }
         }
     }
-    
-    console.log(differences.sort().join(' '));
-} 
+    console.log(result.join(' '))
+}
 
 process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
 process.stdin.setEncoding("ascii");
 _input = "";
 process.stdin.on("data", function (input) {
