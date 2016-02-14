046581cc9d6c7a7da9686a7f8b48839829992b4d
John Jullies Palma <johnjullies@users.noreply.github.com>
2016-02-14 21:47:30 +0800

    Create Day 4: Template Strings.js

---
+++ hackerrank/contests/7-Days-of-JavaScript/Day 4: Template Strings.js
@@ -0,0 +1,5 @@
+// https://www.hackerrank.com/contests/7days-javascript/challenges/js-template-strings
+
+var my_template_string = `My name is ${my_name}.
+My id is ${my_id}.
+My address is ${my_address}.`;
